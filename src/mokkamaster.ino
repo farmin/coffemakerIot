2f50edf4ac21f3643b7237b29697d3c50601fbca
Armin <tilfarmin@gmail.com>
2018-05-24 15:14:19 +0200

    added some comments

--- src/mokkamaster.ino
+++ src/mokkamaster.ino
@@ -1,5 +1,10 @@
+// Simple Demonstration of a coffe level and temperature sensor. Sends
+// mqtt data to broker via Wifi.
+//
+//
 // based on a example by Joël Gähwiler
 // https://github.com/256dpi/arduino-mqtt
+
 #include <Arduino.h>
 #include "EEPROM.h"
 #include <ArduinoJson.h>
@@ -7,60 +12,71 @@
 #include <MQTT.h>
 #include <Adafruit_ADS1015.h>
 #include <Adafruit_MLX90614.h>
-// #include <Button.h>
 
-#define MQTT_MAX_PACKET_SIZE 512
-#define ANALOG_PIN_0 36
-int currentSense = 0;
 
+// i2c sensors initialisation. Ads0-2 is light sensors
+// and mlx is ir temperature sensor.
 Adafruit_ADS1115 ads0(0x48); // address 0b1001000
 Adafruit_ADS1115 ads1(0x49); // address 0b1001001
 Adafruit_ADS1115 ads2(0x4a); // address 0b1001010
 Adafruit_MLX90614 mlx = Adafruit_MLX90614(); // address 5A 
 
+// Constants for wifi connection.
 const char ssid[] = SSID_NAME;
 const char pass[] = SSID_PASWORD;
-
-int addr = 0;
-//#define EEPROM_SIZE 4 * 11
-#define SENSOR_ARRAY_SIZE 11
-#define EEPROM_SIZE SENSOR_ARRAY_SIZE * sizeof(unsigned int) 
 
 WiFiClientSecure net;
 MQTTClient client;
 unsigned long lastMillis = 0;
 
+// Json buffer constants
+StaticJsonBuffer<256> jb;
+JsonObject &obj = jb.createObject();
+char output[128];
+
+// Defines Button and led pins
 const int buttonPin = 23;
 const int buttonDrivePin = 12;
 const int ledPin = 13;
 
 
-#define DEBUG 
-#ifdef DEBUG
-#define DEBUG_PRINT(x)      Serial.print (x)
-#define DEBUG_PRINTln(x)    Serial.println (x)
-#else
-#define DEBUG_PRINT(x)
-#define DEBUG_PRINTln(x)
-#endif
-
-
+// Includes all data from the sensor stick
 struct SensorLevelArray{
     unsigned int fillLevel = 0;
     double tempObjectC = 0;
     double tempAmbientC = 0;
-    const static int thresholdNum = 11;
+    const static int thresholdNum = 12;
     unsigned int sensor[thresholdNum];
     unsigned int sensorThresholdValues[thresholdNum];
+    unsigned int currentSense = 0;
 };
 SensorLevelArray fillArray;
 
-//unsigned int  sensorArrayThreshold[11];
+// EEPROM/Flash store size. Determine number of bytes needed to
+// store reference values.
+#define EEPROM_SIZE fillArray.thresholdNum * sizeof(unsigned int) // <- fix constant
+// pin for current reading with ADC conversion.
+#define ANALOG_PIN_0 36 
 
+
+// Used for debugging.
+#define DEBUG 
+#ifdef DEBUG
+    #define DEBUG_PRINT(x)      Serial.print (x)
+    #define DEBUG_PRINTln(x)    Serial.println (x)
+#else
+    #define DEBUG_PRINT(x)
+    #define DEBUG_PRINTln(x)
+#endif
+
+// Checks if we have a valid connection for Wifi and
+// mqtt broker.
 void connect()
 {
-    DEBUG_PRINT("checking wifi...");
+    // Checks if we got a wifi connection,
+    // reboots system if we fails 15 times.
     uint8_t i = 0;
+    DEBUG_PRINT("checking wifi...");
     while (WiFi.status() != WL_CONNECTED) {
 	DEBUG_PRINT(".");
 	delay(1000);
@@ -70,17 +86,21 @@ void connect()
 	    ESP.restart();
 	}
     }
-    
+
+    // Connects to mqtt broker, hangs if none is
+    // found.
     DEBUG_PRINT("\n\rconnecting...");
     while (!client.connect("mokkamaster", "try", "try")) {
 	DEBUG_PRINT(".");
 	delay(1000);
     }
-
     DEBUG_PRINTln("\n\rconnected!");
     client.subscribe("coffeMaker1");
 }
 
+
+// Message received callback. Does nothing
+// except printing topics and payload.
 void messageReceived(String &topic, String &payload) {
     Serial.println("incoming: " + topic + " - " + payload);
     // client.unsubscribe("coffeMaker1");
@@ -98,21 +118,23 @@ void setup()
 	Serial.println("failed to initialise EEPROM");
 	delay(1000000);
     }
-     Serial.println(" bytes read from Flash . Values are:");
-     //    for (int i = 0; i < EEPROM_SIZE; ++i)
-     for (int i = 0; i < SENSOR_ARRAY_SIZE ; i++)
+    Serial.println(" bytes read from Flash . Values are:");
+    //    for (int i = 0; i < EEPROM_SIZE; ++i)
+    for (int i = 0; i < fillArray.thresholdNum ; i++)
     {
 	Serial.print("eeprom :" + String(i) + " : ");
 	Serial.println(EEPROM.readUInt(i*4));
 	
     }
-    
+    // initialize i2c bus and sensors.
     ads0.begin();
     ads1.begin();
     ads2.begin();
     mlx.begin();
 
+    // starts Wifi connection.
     WiFi.begin(ssid, pass);
+    
     // MQTT brokers usually use port 8883 for secure connections.
     //client.begin("broker.shiftr.io", 8883, net);
     client.begin("192.168.188.235", 8883, net);
@@ -128,14 +150,10 @@ int getCoffeLevel(SensorLevelArray *sensorArray ){
     }
 }
 
-// int getCoffeLevel(unsigned int *sensors, int arrayLength){
-//     for (uint8_t i = 0; i < SENSOR_ARRAY_SIZE ; i++ ){
-// 	Serial.printf("sensor %1d has value %5d\r\n", i, sensors[i]);
-// 	//Serial.println (sensors[i]);
-//     }
-// }
-
+// Loops trough i2c adc, and updates values
+// in struct sensorArray
 int getSensorData(SensorLevelArray *sensorArray){
+   
     for (uint8_t i = 0; i != sensorArray->thresholdNum ; i++ ){
 	if (i >= 0 && i <= 3){
 	    sensorArray->sensor[i] = ads0.readADC_SingleEnded(i);
@@ -150,42 +168,25 @@ int getSensorData(SensorLevelArray *sensorArray){
 	    Serial.print("error");
 	}
     }
-    
 }
 
-// int getSensorData(unsigned int *sensors, int arrayLength){
-//     /* for (uint8_t i = 0; i < sizeof(sensors) / sizeof(sensors[0]) + 1 ; i++ ){ */
-//     for (uint8_t i = 0; i != arrayLength ; i++ ){
-// 	if (i >= 0 && i <= 3){
-// 	    sensors[i] = ads0.readADC_SingleEnded(i);
-// 	}
-// 	else if( i >= 3  && i <= 7 ){
-// 	    sensors[i] = ads1.readADC_SingleEnded(i - 4);
-// 	}
-// 	else if (i >= 7 ){
-// 	    sensors[i] = ads2.readADC_SingleEnded(i - 8);
-// 	}
-// 	else{
-// 	    Serial.print("error");
-// 	}
-//     }
-// }
-
+// Updates threshold values stored in EEPROM/FLASH. Theese
+// Are used when we determine fill level. ButtonPin is used when we
+// store settings.
 void configMode(SensorLevelArray *sensorArray){
-    //  TODO ==> get some eeprom and stor values permanently, this have to do for now.
+    // <=TODO give some visulal feedback when user isn't connected via terminal.
+    // <=TODO fix fix hang forever, if user dosent complete setup.
     int serialByte = 0;
     Serial.println("This is for configuring you brand new coffee machine iot sensor.");
     Serial.println("Please place a empty coffe container on the hotplate and press");
     Serial.println("button.");
-    delay(3000);
-
+    delay(1000);
     while( digitalRead(buttonPin) != HIGH)
     {
 	Serial.print('.');
 	delay(250);
     }
     SensorLevelArray sensorsEmptyContainer;
-
     getSensorData(&sensorsEmptyContainer);
     Serial.println("Now fill the container with coffe and press the button again");
     delay(1000);
@@ -207,101 +208,57 @@ void configMode(SensorLevelArray *sensorArray){
 }
 
 
-// void configMode(unsigned int *sensorThresholdValues, int arrayLength){
-//     //  TODO ==> get some eeprom and stor values permanently, this have to do for now.
-//     int serialByte = 0;
-//     Serial.println("This is for configuring you brand new coffee machine iot sensor.");
-//     Serial.println("Please place a empty coffe container on the hotplate and press");
-//     Serial.println("button.");
-//     delay(3000);
-
-//     while( digitalRead(buttonPin) != HIGH)
-//     {
-// 	Serial.print('.');
-// 	delay(250);
-//     }
-//     unsigned int sensorsWhenLit[11];
-//     getSensorData(sensorsWhenLit, 12);
-//     Serial.println("Now fill the container with coffe and press the button again");
-//     delay(1000);
-//     while( digitalRead(buttonPin) != HIGH)
-//     {
-// 	Serial.print('.');
-// 	delay(250);
-//     }
-//     unsigned int sensorsWhenCoffe[11];
-//     //float cf = 0;
-//     unsigned int cf = 0;
-//     getSensorData(sensorsWhenCoffe, 12);
-//     for(int i = 0; i < arrayLength; i++ )
-//     {
-// 	// dLow + dHigh / 2
-// 	cf = ((sensorsWhenLit[i] + sensorsWhenCoffe[i])) ;
-// 	cf = cf / 2 ;
-// 	sensorThresholdValues[i] = cf;
-//     }
-// }
-
-
-StaticJsonBuffer<256> jb;
-JsonObject &obj = jb.createObject();
-char output[128];
-
-
+// main program loop
 void loop() {
 
     delay(200);
     if(digitalRead(buttonPin) == HIGH){
-	//configMode(sensorArrayThreshold, 12);
 	configMode(&fillArray);
 	for( int i = 0; i != 12; ++i){
-	    EEPROM.writeUInt(i * sizeof(fillArray.sensorThresholdValues[i]),
-			     fillArray.sensorThresholdValues[i]);
+	    EEPROM.writeUInt(i * sizeof(fillArray.sensorThresholdValues[i]),  fillArray.sensorThresholdValues[i]);
 	    EEPROM.commit();
 	}
     }
 
     client.loop();
-    delay(1000);  // <- fixes some issues with WiFi stability
+    delay(1000);  // <- should fix some issues with WiFi stability
     if (!client.connected()){
 	connect();
     }
+    //
+    //Only runs when millis overflows.
+    //This is our main loop.
     if (millis() - lastMillis > 2000) {
 	delay(10);
-
-	for( int i = 0; i < SENSOR_ARRAY_SIZE ; ++i){
+	for( int i = 0; i < fillArray.thresholdNum ; ++i){
 	    DEBUG_PRINT("eeprom :" + String(i) + " : ");
-	    DEBUG_PRINTln(EEPROM.readUInt(i* sizeof(sensorArrayThreshold[i])));
+	    DEBUG_PRINTln(EEPROM.readUInt(i* sizeof(fillArray.sensorThresholdValues[i])));
 	}
-
-	currentSense = analogRead(ANALOG_PIN_0); // this needs a filter or something
+	// Checks if coffemaker draws current
+	fillArray.currentSense = analogRead(ANALOG_PIN_0); // this needs a filter or something
 	// DEBUG_PRINTln(currentSense);
-		
-	// unsigned int lightSensorArray[11];
-	// getSensorData(lightSensorArray, 12);
-	//getCoffeLevel(lightSensorArray, 12);
 
 	getSensorData(&fillArray);
 	getCoffeLevel(&fillArray);
 	fillArray.tempObjectC =  mlx.readObjectTempC();
 	fillArray.tempAmbientC = mlx.readAmbientTempC();
-	
-	// char toc[8];
-	// char tac[8];
-	// double tempObjectC = mlx.readObjectTempC();
-	// double tempAmbientC = mlx.readAmbientTempC();
-	// dtostrf(tempObjectC, 3, 1, toc);
-	// dtostrf(tempAmbientC, 3, 1, tac);
-	// DEBUG_PRINT("object temp: ");
-	// DEBUG_PRINTln(toc);
-	// DEBUG_PRINT("ambient temp: ");
-	// DEBUG_PRINTln(tac);
-	obj["currentSense"] = currentSense;
-	obj["CoffeLevel"] = 1;
+	//
+	// Compares sensorvalue with reference value and sets coffeLevel count accordingly.
+	int coffeLevel = 0;
+	int i = 0;
+	for( sensorValue : fillArray.sensor){
+	    if (sensorValue > fillArray.sensorThresholdValues[i]){ coffeLevel++ ;}
+	    i++;
+	}
+	fillArray.fillLevel = coffeLevel;
+
+	//Updates Jsonbuffer with current values
+	//and sends mqtt message
+	obj["currentSense"] = fillArray.currentSense;
+	obj["CoffeLevel"] = fillArray.fillLevel;
 	obj["PerculatorOnTimer"].set(42);
 	obj["coffeTemp"].set(fillArray.tempObjectC);
 	obj["ambientTemp"].set(fillArray.tempAmbientC);
-	
 	obj.printTo(output);
 	Serial.println(output);
 	client.publish("coffeMaker1", output);
